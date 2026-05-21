c5da2a10201d3cf79d5d958073596e148a866268
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 19:48:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from flask import Flask
-from kiteconnect import KiteConnect
-import os
 
 app = Flask(__name__)
 
-API_KEY = os.getenv("KITE_API_KEY")
-
-kite = KiteConnect(api_key=API_KEY)
-
 @app.route("/")
 def home():
-    return f'<a href="{kite.login_url()}">Login to Zerodha</a>'
+    return "Bot Running Successfully"

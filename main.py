07bf1208d2c953786a4ac7ce1a11450385b022b2
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 12:14:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-webhook
+Python
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+BOT_TOKEN = "8213688201:AAFJVt9dLyAUPfnh_eWetpMmzbaS_Nb4tk"
+CHAT_ID = "-1003932123947"
+@app.route('/webhook', methods=['POST'])
+def webhook():
+  data = request.json
+  symbol = data.get("symbol", "SENSEX")
+  signal = data.get("signal", "BUY")
+  price = data.get("price", "0")
+  message = f"""
+  📊 Sensex Alert
+  📈 Symbol: {symbol}
+  🚀 Signal: {signal}
+  💰 Price: {price} """
+  telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+  payload = {
+    "chat_id": CHAT_ID,
+    "text": message }
+  requests.post(telegram_url, json=payload)
+  return {"status": "success"}
+  @app.route('/')
+  def home():
+    return "Bot is running"
+    if __name__ == "__main__":
+      app.run(host='0.0.0.0', port=10000)

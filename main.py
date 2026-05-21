0f73ca443168e45a825f86de56cd8f6f78a4d4ae
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 21:31:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ kite = KiteConnect(api_key=API_KEY)
 kite.set_access_token(ACCESS_TOKEN)
 
 # ADD TELEGRAM FUNCTION HERE
-def if signal != "NO SIGNAL":
+    if signal != "NO SIGNAL":
     send_telegram_message(message)
 
     token = os.getenv("TELEGRAM_TOKEN")

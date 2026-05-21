73afe2f05edee5c5f762d5077b48c01c797eaf95
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 19:39:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ import os
 app = Flask(__name__)
 
 API_KEY = os.getenv("KITE_API_KEY")
-API_SECRET = os.getenv("KITE_API_SECRET")
 
 kite = KiteConnect(api_key=API_KEY)
 
 @app.route("/")
 def home():
-    login_url = kite.login_url()
-    return f'<a href="{login_url}">Login to Zerodha</a>'
+    return f'<a href="{kite.login_url()}">Login to Zerodha</a>'

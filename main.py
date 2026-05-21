f589ca3aee63e7f81cf0031f0b7f2ae40b65e2b3
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 22:13:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,8 @@ EMA21: {latest['EMA21']}
 
 if signal != "NO SIGNAL":
     send_telegram_message(message)
-    return f"""
+
+return f"""
     
     Signal: {signal}<br><br>
     Price: {latest['close']}<br>

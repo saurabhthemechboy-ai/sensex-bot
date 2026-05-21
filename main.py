8706c112a0b16b74d30761cbd587642c4164dfd0
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 20:56:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from kiteconnect import KiteConnect
 import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -15,8 +16,33 @@ kite.set_access_token(ACCESS_TOKEN)
 @app.route("/")
 def home():
 
-    data = kite.quote("BSE:SENSEX")
+    # Get historical data
+    data = kite.historical_data(
+        instrument_token=265,
+        from_date="2026-05-20",
+        to_date="2026-05-21",
+        interval="5minute"
+    )
 
-    last_price = data["BSE:SENSEX"]["last_price"]
+    df = pd.DataFrame(data)
 
-    return f"Live Sensex Price: {last_price}"
+    # EMA calculations
+    df["EMA9"] = df["close"].ewm(span=9).mean()
+    df["EMA21"] = df["close"].ewm(span=21).mean()
+
+    latest = df.iloc[-1]
+
+    signal = "NO SIGNAL"
+
+    if latest["EMA9"] > latest["EMA21"]:
+        signal = "BUY"
+
+    elif latest["EMA9"] < latest["EMA21"]:
+        signal = "SELL"
+
+    return f"""
+    Signal: {signal}<br><br>
+    Price: {latest['close']}<br>
+    EMA9: {latest['EMA9']}<br>
+    EMA21: {latest['EMA21']}
+    """

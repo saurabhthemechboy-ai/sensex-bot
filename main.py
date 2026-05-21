66334739e8101002bcb117cb0f29662672de25b0
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 20:42:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import os
 app = Flask(__name__)
 
 API_KEY = os.getenv("KITE_API_KEY")
-ACCESS_TOKEN = "PASTE_YOUR_ACCESS_TOKEN"
+
+ACCESS_TOKEN = "f3yAMI1PMBgORimWDAQS0ViEGk4WmT36"
 
 kite = KiteConnect(api_key=API_KEY)
+
 kite.set_access_token(ACCESS_TOKEN)
 
 @app.route("/")
@@ -15,8 +17,8 @@ def home():
 
     profile = kite.profile()
 
-    return f"""
-    Connected Successfully<br><br>
-    User Name: {profile['user_name']}<br>
-    User ID: {profile['user_id']}
-    """login_url()}">Login to Zerodha</a>'
+    return (
+        f"Connected Successfully<br><br>"
+        f"User Name: {profile['user_name']}<br>"
+        f"User ID: {profile['user_id']}"
+    )

5d55f7b6d9c6a4df35bb935aaedfc1b7aaaafe4c
saurabhthemechboy-ai <saurabhthemechboy@gmail.com>
2026-05-21 19:54:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from kiteconnect import KiteConnect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot Running Successfully"
+    kite = KiteConnect(api_key="test")
+    return "Kite SDK Working"
